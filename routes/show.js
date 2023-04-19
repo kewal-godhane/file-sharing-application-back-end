32cb0ec699014d5d790cd352e28d7cf66032994e
kewal godhane <kewalgodhane008@gmail.com>
2023-04-19 21:22:01 +0530

    url change

--- routes/show.js
+++ routes/show.js
@@ -11,7 +11,7 @@ router.get('/:uuid', async (req, res) => {
         return res.render('download', { uuid: file.uuid, 
             fileName: file.filename, 
             fileSize: file.size, 
-            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}` });
+            downloadLink: `https://file-sharing-application-by-kewal.onrender.com/files/download/${file.uuid}` });
             // http://localhost:3000/files/download/slasjflldkf-29ksjfknf
     } catch(err) {
         return res.render('download', { error: 'Something went wrong.'});
